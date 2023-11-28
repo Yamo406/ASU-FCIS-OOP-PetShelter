ad13b7c7dfc609dfc834bd2718cbfa14d0e9f605
Yamo <mohamadd.abdelnaby@gmail.com>
2023-11-28 03:49:23 +0200

    Create Admin Class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import User.*;
+import Room.*;
+import Pet.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+    }
+}
---
+++ src/User/Admin.java
@@ -0,0 +1,51 @@
+package User;
+
+public class Admin extends User
+{
+
+  public void AddPets()
+  {
+
+  }
+
+  public void DeleteEntity()
+  {
+
+  }
+
+  public void DisplayAvailableOperationSlots()
+  {
+
+  }
+
+  public void DisplayDonorData()
+  {
+
+  }
+
+  public void DisplayVetData()
+  {
+
+  }
+
+  public void DisplayPetData()
+  {
+
+  }
+
+  public void DisplayReadyForAdoptionPets()
+  {
+
+  }
+
+  public void CalulateDonated()
+  {
+
+  }
+
+  public void UpdatePetData()
+  {
+    
+  }
+  
+}
