9c5cac9f43802a7a1dad337407ce1bd57f638605
Randern212 <128811970+Randern212@users.noreply.github.com>
2023-12-10 23:53:54 +0200

    Add files via upload

---
+++ src/User/User.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+public abstract class User implements Comparable {
+    protected String name;
+    protected String password;
+    protected static ArrayList<User> registeredUser= new ArrayList<>();
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+    public void Register(User user)
+    {
+        for(User item:registeredUser)
+        {
+            if (user.compareTo(item)==0)
+            {
+                System.out.println("Invalid username or password");
+            }
+            else
+            {
+                registeredUser.add(user);
+            }
+        }
+    }
+    public abstract void Login();
+
+    @Override
+    public int compareTo(Object o)
+    {
+        User item=(User) o;
+        if (this.name== item.name)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
