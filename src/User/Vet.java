852dcfe32d2eb0d8678894310af35ec143a68c25
Randern212 <128811970+Randern212@users.noreply.github.com>
2023-12-11 20:29:02 +0200

    Add files via upload

---
+++ src/User/Vet.java
@@ -0,0 +1,52 @@
+public class Vet eximport pet;
+tends User {
+    private int yrs_exp; //years of experience
+    private int vetId;
+    private Pet[] pets;
+
+    public Vet(String username, String password, int yrs_exp, int vetId) {
+        super(username, password);
+        this.yrs_exp = yrs_exp;
+        this.vetId = vetId;
+        this.pets = new Pet[20];
+    }
+
+    public void Login(){
+
+    }
+
+    public void prescription(int pet_id,Donation donation)
+    {
+
+
+    }
+
+    public void requestAppointment() {
+
+
+    }
+
+    public void addPet(Pet pet) {
+        //check if there's space to add the pet
+
+
+
+
+    }
+
+    public void updatePetStatus(Pet p,Treatment_Status TS)
+    {
+        for(Pet item:pets)
+        {
+            if(item.compareTo(p)==0)
+            {
+                if(TS == Treatment_Status.Treated)
+                {
+                    p.setReadyForAdoptionStatus(Pet.Ready_for_adoption_status.Ready);
+
+                }
+
+            }
+        }
+    }
+}
