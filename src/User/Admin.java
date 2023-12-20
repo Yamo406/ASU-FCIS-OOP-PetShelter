0c82355ef43738e47e3f42b1b88c3bc2647b3c00
Randern212 <128811970+Randern212@users.noreply.github.com>
2023-12-20 10:04:29 +0200

    Update Admin.java

--- src/User/Admin.java
+++ src/User/Admin.java
@@ -38,44 +38,6 @@ public class Admin extends User
     String pTreatment=petInput.next();
     Pet addTarget=new Pet(pName,Pet_type.valueOf(pType),pAge,Adoption_status.valueOf(pAdoption_status),Ready_for_adoption_status.valueOf(pReady),Treatment_Status.valueOf(pTreatment));
     Pet.shelteredPets.add(addTarget.getID(),addTarget);
-//    String filePath = "Pet/petsData.txt";
-//    try
-//    {
-//      FileWriter fileWriter = new FileWriter(filePath);
-//      BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-//      Pet pet = new Pet(filePath, null, 0, null, null, null);
-//      System.out.println("Please inter the data of the pet.");
-//
-//      System.out.println("Pet Name:");
-//      pet.setName(scanner.nextLine());
-//
-//      System.out.println("Pet Age:");
-//      pet.setAge(scanner.nextInt());
-//      scanner.nextLine();
-//
-//      System.out.println("Pet Type (Cat or Dog):");
-//      String petTypeString = scanner.nextLine().toUpperCase();
-//      Pet_type pet_type = Pet_type.valueOf(petTypeString);
-//      pet.setType(pet_type);
-//
-//      System.out.println("Pet Adoption Status (Adopted or Not_Adopted):");
-//      String petAdoptionStatus = scanner.nextLine().toUpperCase();
-//      Adoption_status adoption_status = Adoption_status.valueOf(petAdoptionStatus);
-//      pet.setAdoptionStatus(adoption_status);
-//
-//      System.out.println("Pet Treatment Status: (Sick, recovering, or treated)");
-//      String petTreatmentStatus = scanner.nextLine().toUpperCase();
-//      Treatment_Status treatment_Status = Treatment_Status.valueOf(petTreatmentStatus);
-//      pet.setTreatmentStatus(treatment_Status);
-//
-//      bufferedWriter.write(pet.toString());
-//      bufferedWriter.newLine();
-//
-//      bufferedWriter.close();
-//      fileWriter.close();
-//    } catch (Exception e) {
-//      e.printStackTrace();
-//    }
   }
 
   public void DeleteEntity()
@@ -124,65 +86,6 @@ public class Admin extends User
         break;
     }
   }
-
-
-//  public void DeleteEntity(String filepath, int deleteLine)
-//  {
-//    char answer;
-//    System.out.println("Delete entity?");
-//    do
-//    {
-//      System.out.println("Please enter your choice: (y/n)");
-//      answer = scanner.next().charAt(0);
-//
-//    } while (answer != 'y' || answer != 'Y' || answer != 'N' || answer != 'n');
-//    if (answer == 'y' || answer == 'Y')
-//    {
-//      String tempFile = "temp.txt";
-//      File oldFile = new File(filepath);
-//      File newFile = new File(tempFile);
-//      int line = 0;
-//      String currentLine;
-//      try {
-//        FileWriter fw = new FileWriter(tempFile, true);
-//        BufferedWriter bw = new BufferedWriter(fw);
-//        PrintWriter pw = new PrintWriter(bw);
-//
-//        FileReader fr = new FileReader(filepath);
-//        BufferedReader br = new BufferedReader(fr);
-//
-//        while((currentLine = br.readLine()) != null)
-//        {
-//          line++;
-//
-//          if (deleteLine != line) {
-//            pw.println(currentLine);
-//          }
-//        }
-//
-//        pw.flush();
-//        pw.close();
-//        fr.close();
-//        br.close();
-//        bw.close();
-//        fw.close();
-//
-//        oldFile.delete();
-//        File dump = new File(filepath);
-//        newFile.renameTo(dump);
-//
-//      } catch (Exception e) {
-//        System.out.println("Error in deleting line");
-//      }
-//
-//    }
-//    else if (answer == 'N' || answer == 'n')
-//    {
-//      System.out.println("Okay! Exiting the program");
-//      System.exit(1);
-//    }
-//  }
-
   public void DisplayAvailableOperationSlots()
   {
     for (operation room : operation.availableRoom) {
