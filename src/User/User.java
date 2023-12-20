97d53dbae835ef94a76fce22cb74b5af4d042154
Randern212 <128811970+Randern212@users.noreply.github.com>
2023-12-20 07:02:28 +0200

    Update User.java

--- src/User/User.java
+++ src/User/User.java
@@ -1,28 +1,19 @@
-package User;
-
-import Exceptions.*;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 public abstract class User implements Comparable {
     protected String name;
     protected String password;
-    protected int user_index;
-    protected static int no_users=0;
-    protected static ArrayList<User> registeredUser= new ArrayList<>(10);
+    public static ArrayList<User> registeredUser= new ArrayList<>();
     public User(String name, String password) {
         this.name = name;
         this.password = password;
-        user_index=no_users;
-        no_users++;
     }
 
     public User() {
 
     }
 
-    public abstract void Register (User user) throws RegisterException, IOException;
+    public abstract void Register (User user) throws RegisterException;
     public static boolean Login(String Name,String Password) throws LoginException
     {
             for(User item: registeredUser)
@@ -67,9 +58,5 @@ public abstract class User implements Comparable {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getUser_index() {
-        return user_index;
     }
 }
